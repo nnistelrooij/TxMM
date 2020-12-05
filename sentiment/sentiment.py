92403abd3d98237d1f062115981e41d4241d3645
ovacia345 <nielsvann@hotmail.nl>
2020-12-05 23:09:29 +0100

    Add console argument

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -36,8 +36,8 @@ if __name__ == '__main__':
     comments = pd.read_csv('../clean/clean_comments.csv')
     comments = comments.assign(text=comments['text'].str.replace('\"', '\\\"'))
 
-    num_comments = sys.argv[1]
-    command_comments = comments[:num_comments]
+    num_comments = sys.argv[1] if len(sys.argv) == 2 else comments.shape[0]
+    command_comments = comments[:range(num_comments)]
 
     command_comments['sentiment'] = get_sentiment(command_comments['text'])
     command_comments.to_csv('output.csv', index=False)
